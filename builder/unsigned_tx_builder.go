5b711cd093351a78d65ea0cb6fae40f3f66588f8
shaojunda <shaojunda@gmail.com>
2020-09-09 08:27:27 +0800

    chore: return error on builder methods

--- builder/unsigned_tx_builder.go
+++ builder/unsigned_tx_builder.go
@@ -1,17 +1,18 @@
 package builder
 
 import (
+	"github.com/coinbase/rosetta-sdk-go/types"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	ckbTypes "github.com/nervosnetwork/ckb-sdk-go/types"
 )
 
 // UnsignedTxBuilder is an interface for different script unsignedTxBuilder
 type UnsignedTxBuilder interface {
-	BuildVersion() hexutil.Uint
-	BuildCellDeps() []ckbTypes.CellDep
-	BuildHeaderDeps() []ckbTypes.Hash
-	BuildInputs() []ckbTypes.CellInput
-	BuildOutputs() []ckbTypes.CellOutput
-	BuildOutputsData() [][]byte
-	BuildWitnesses() [][]byte
+	BuildVersion() (hexutil.Uint, *types.Error)
+	BuildCellDeps() ([]ckbTypes.CellDep, *types.Error)
+	BuildHeaderDeps() ([]ckbTypes.Hash, *types.Error)
+	BuildInputs() ([]ckbTypes.CellInput, *types.Error)
+	BuildOutputs() ([]ckbTypes.CellOutput, *types.Error)
+	BuildOutputsData() ([][]byte, *types.Error)
+	BuildWitnesses() ([][]byte, *types.Error)
 }
