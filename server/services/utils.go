f67867eacc78d99690232e517db48d4bf5e3e46a
shaojunda <shaojunda@gmail.com>
2020-09-29 21:53:38 +0800

    feat: add getConstructionType function

--- server/services/utils.go
+++ server/services/utils.go
@@ -43,12 +43,71 @@ func wrapErr(rErr *types.Error, err error) *types.Error {
 }
 
 func separateInputAndOutput(operations []*types.Operation) (inputOperations []*types.Operation, outputOperations []*types.Operation) {
-	inputOperations = operationFilter(operations, func(operation *types.Operation) bool {
-		return operation.Type == ckb.InputOpType
-	})
-	outputOperations = operationFilter(operations, func(operation *types.Operation) bool {
-		return operation.Type == ckb.OutputOpType
-	})
+	inputOperations = getInputOperations(operations)
+	outputOperations = getOutputOperations(operations)
 
 	return
+}
+
+func getInputOperations(operations []*types.Operation) []*types.Operation {
+	return operationFilter(operations, func(operation *types.Operation) bool {
+		return operation.Type == ckb.InputOpType
+	})
+}
+
+func getOutputOperations(operations []*types.Operation) []*types.Operation {
+	return operationFilter(operations, func(operation *types.Operation) bool {
+		return operation.Type == ckb.OutputOpType
+	})
+}
+
+func getConstructionType(operations []*types.Operation, signatures []*types.Signature, cfg *config.Config) (string, *types.Error) {
+	inputOperations := getInputOperations(operations)
+	outputOperations := getOutputOperations(operations)
+	if ok, err := isTransferCKB(inputOperations, outputOperations, signatures, cfg); ok {
+		if err != nil {
+			return "", err
+		}
+		return ckb.TransferCKB, nil
+	} else {
+		return "", UnsupportedConstructionTypeError
+	}
+}
+
+func isTransferCKB(inputOperations []*types.Operation, outputOperations []*types.Operation, signatures []*types.Signature, cfg *config.Config) (bool, *types.Error) {
+	if signatures == nil {
+		for _, operation := range inputOperations {
+			parsedAddress, err := address.Parse(operation.Account.Address)
+			if err != nil {
+				return false, AddressParseError
+			}
+			if parsedAddress.Script.CodeHash.String() != cfg.Secp256k1Blake160.Script.CodeHash || string(parsedAddress.Script.HashType) != cfg.Secp256k1Blake160.Script.HashType {
+				return false, nil
+			}
+		}
+
+		for _, operation := range outputOperations {
+			var metadata ckb.OperationMetadata
+			err := types.UnmarshalMap(operation.Metadata, &metadata)
+			if err != nil {
+				return false, wrapErr(DataParseError, err)
+			}
+			if metadata.Type != nil {
+				return false, nil
+			}
+		}
+	} else {
+		var metadata ckb.AccountIdentifierMetadata
+		for _, signature := range signatures {
+			err := types.UnmarshalMap(signature.SigningPayload.AccountIdentifier.Metadata, &metadata)
+			if err != nil {
+				return false, wrapErr(InvalidAccountIdentifierMetadataError, err)
+			}
+			if metadata.LockType != ckb.Secp256k1Blake160Lock.String() {
+				return false, nil
+			}
+		}
+	}
+
+	return true, nil
 }
